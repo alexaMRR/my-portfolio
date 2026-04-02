7170cd9dcacda3fb1763930242ff0e4ff23cc4d7
alexaMRR <44932717+alexaMRR@users.noreply.github.com>
2026-04-01 20:07:34 -0700

    Update Heropage.jsx

--- src/components/Heropage.jsx
+++ src/components/Heropage.jsx
@@ -7,7 +7,7 @@ export default function HeroPage({ navigate }) {
     <div className="hero-page">
 
     <div class = "hero-signature">
-        <img src= "src\assets\signaturePlaceholder.png"/>
+        <img src= "src/components/signaturePlaceholder.png"/>
     </div>
 
     <div className="hero-top">
